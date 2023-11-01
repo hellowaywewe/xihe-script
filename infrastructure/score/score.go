bcf0dde442c9bf49e1c73846ac8afb1bd4fe7819
drizzlezyk <drizzlezyk@163.com>
2023-11-01 09:44:54 +0800

    fix: param deliver make code clean

--- infrastructure/score/score.go
+++ infrastructure/score/score.go
@@ -22,16 +22,24 @@ func NewEvaluateScore(evaluate string) score.EvaluateScore {
 }
 
 func (s *evaluateImpl) Evaluate(col *message.MatchFields, cfg *config.OBSConfig) (data []byte, err error) {
-	args := []string{"python3", s.evaluate, "--pred_path", col.Path, "--true_path", col.AnswerPath, "--cls", strconv.Itoa(col.Cls), "--pos", strconv.Itoa(col.Pos)}
-	input := fmt.Sprintf("%s+%s+%s+%s\n", cfg.AccessKey, cfg.SecretKey, cfg.Bucket, cfg.Endpoint)
-	var b bytes.Buffer
-	b.Write([]byte(input))
-	c, err, _ := utils.RunCmd(&b, args...)
+	args := []string{
+		"python3", s.evaluate,
+		"--pred_path", col.Path,
+		"--true_path", col.AnswerPath,
+		"--cls", strconv.Itoa(col.Cls),
+		"--pos", strconv.Itoa(col.Pos),
+	}
+
+	b := bytes.NewBuffer([]byte(fmt.Sprintf(
+		"%s+%s+%s+%s\n",
+		cfg.AccessKey, cfg.SecretKey, cfg.Bucket, cfg.Endpoint,
+	)))
+
+	data, err, _ = utils.RunCmd(b, args...)
 	if err != nil {
 		return
 	}
 
-	data = []byte(c)
 	data = bytes.ReplaceAll(bytes.TrimSpace(data), []byte(`'`), []byte(`"`))
 
 	return
